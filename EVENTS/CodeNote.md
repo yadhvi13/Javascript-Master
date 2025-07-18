fdceaa075e3c3f4b9752fedfd551e563b19a712c
Yogeeta <yadhvi.ch@gmail.com>
2025-07-18 21:45:08 +0530

    EVent Handling

--- EVENTS/CodeNote.md
+++ EVENTS/CodeNote.md
@@ -64,4 +64,70 @@ inp.addEventListener("input", function(e){
 })
 
 //CHANGE EVENT
+let sel = document.querySelector("select");
+let device = document.querySelector("#device");
+
+sel.addEventListener("change", function(e){
+    // console.log(e.target.value);
+    // device.textContent = "device selected";
+    device.textContent = `${e.target.value} Device Selected`;
+})
+
+//custom input file button using JS and also displaying the file name which is being selected.
+
+let inp = document.querySelector("#inp")
+document.querySelector("#fileinp");
+
+inp.addEventListener("click",function(){
+     fileinp.click();
+})
+
+fileinp.addEventListener("change", function(details){
+//  console.log(details.target.files[0].name);
+  const file = details.target.files[0];
+  if(details.target.files){
+    inp.textContent = file.name;
+ }
+})
+
+// Prevent page from Reload
+let form = document.querySelector("form");
+form.addEventListener("submit",function(details){
+     details.preventDefault();
+})
+
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main = document.querySelector("#main");
+
+form.addEventListener("submit",function(details){
+     details.preventDefault();
+
+ //Creating Div-1 
+    let card = document.createElement("div");
+    card.classList.add("card");
+ //Creating Div-2
+    let profilepic = document.createElement("div");
+    profilepic.classList.add("profilepic");
+//create image
+    let img = document.createElement("img");
+    img.setAttribute("src",inputs[0].value );
+//Creating H3
+    let h3 = document.createElement("h3");
+    h3.textContent = inputs[1].value;
+//Creating H5
+    let h5 = document.createElement("h5");
+    h5.textContent = inputs[2].value;
+//Creating para tag
+    let p = document.createElement("p");
+    p.textContent = inputs[3].value;
+
+    profilepic.appendChild(img);
+    card.appendChild(profilepic);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);
+    
+    main.append(card);
+})
 ```
--- EVENTS/script.js
+++ EVENTS/script.js
@@ -48,11 +48,80 @@
 //    }  
 // })
 
-let sel = document.querySelector("select");
-let device = document.querySelector("#device");
+// let sel = document.querySelector("select");
+// let device = document.querySelector("#device");
+
+// sel.addEventListener("change", function(e){
+//     // console.log(e.target.value);
+//     // device.textContent = "device selected";
+//     device.textContent = `${e.target.value} Device Selected`;
+// })
+
+// let h1 = document.querySelector("h1");
+// window.addEventListener("keydown", function(dets){
+//     // console.log(dets.key);
+//     // h1.textContent = dets.key;
+//     if(dets.key === " "){
+//         h1.textContent = "SPC";
+//     }
+//     else{
+//         h1.textContent = dets.key;
+//     }
+// })
+
+// let inp = document.querySelector("#inp")
+// document.querySelector("#fileinp");
+
+// inp.addEventListener("click",function(){
+//      fileinp.click();
+// })
+
+// fileinp.addEventListener("change", function(details){
+// //  console.log(details.target.files[0].name);
+//   const file = details.target.files[0];
+//   if(details.target.files){
+//     inp.textContent = file.name;
+//  }
+// })
+
+let form = document.querySelector("form");
+let inputs = document.querySelectorAll("input");
+let main = document.querySelector("#main");
+
+form.addEventListener("submit",function(details){
+     details.preventDefault();
+
+ //Creating Div-1 
+    let card = document.createElement("div");
+    card.classList.add("card");
+ //Creating Div-2
+    let profilepic = document.createElement("div");
+    profilepic.classList.add("profilepic");
+//create image
+    let img = document.createElement("img");
+    img.setAttribute("src", inputs[0].value);
+//Creating H3
+    let h3 = document.createElement("h3");
+    h3.textContent = inputs[1].value;
+//Creating H5
+    let h5 = document.createElement("h5");
+    h5.textContent = inputs[2].value;
+//Creating para tag
+    let p = document.createElement("p");
+    p.textContent = inputs[3].value;
 
 
-sel.addEventListener("change", function(e){
-    // console.log(e.target.value);
-    device.textContent = "device selected";
+    profilepic.appendChild(img);
+    card.appendChild(profilepic);
+    card.appendChild(h3);
+    card.appendChild(h5);
+    card.appendChild(p);
+    
+    main.append(card);
+
+    inputs.forEach(function(inp){
+        if(inp.type !== "submit"){
+            inp.value = "";
+        }
+    });
 })
